4dedc764b0121900227a78ba6065364773cae23d
Edinorogeck <goncharov.1998@mail.ru>
2023-07-13 01:51:17 +0300

    Третий коммит

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import pandas as pd
 
 # GLOBALS PARAMETRS
 
-# Массив для правильных DataFrame pandas
+# Массив для рабочих DataFrame pandas
 df_MKIO = []
 df_ETH = []
-# Массив названий МКИО сообщений
+# Массив для итоговых DataFrame pandas
+df_Total_MKIO = []
+df_Total_ETH = []
+# Массив названий МКИО и ETH сообщений
 arrayMessageNames_MKIO = []
 arrayMessageNames_ETH = []
+# Массив адресов МКИО и ETH сообщений
+arrayMessageAddress_MKIO = []
+arrayMessageAddress_ETH = []
+# Массив с полезными данными МКИО и ETH сообщений
+arrayMessageData_MKIO = []
+arrayMessageData_ETH = []
 
 
 # Функция проверки на вхождение подстроки "МКИО."
@@ -55,7 +64,7 @@ def GetArrayMessageNames_MKIO(df_MKIO):
                 arrayMessageNames_MKIO.append(messageName)
                 break
 
-# Функция создания списка имен сообщений Ethernet
+# Функция создания списка имен сообщений ETH
 def GetArrayMessageNames_ETH(df_ETH):
     # Алгоритм создания списка имен сообщений Ethernet
     for i, table_ETH in enumerate(df_ETH):
@@ -72,8 +81,48 @@ def GetArrayMessageNames_ETH(df_ETH):
                     messageName = GetMessageName(text)
                     break
             if messageName != '':
-                arrayMessageNames_MKIO.append(messageName)
+                arrayMessageNames_ETH.append(messageName)
                 break
+
+# Функция создания списка адресов сообщений МКИО
+def GetArrayMessageAddress_MKIO(df_MKIO):
+    # Алгоритм создания списка имен сообщений МКИО
+    for i, table_MKIO in enumerate(df_MKIO):
+        arrayMessageAddress_MKIO.append(table_MKIO.loc[1, 2].split(None, 1)[0])
+
+# Функция создания списка адресов сообщений ETH
+def GetArrayMessageAddress_ETH(df_ETH):
+    # Алгоритм создания списка имен сообщений МКИО
+    for i, table_ETH in enumerate(df_ETH):
+        addressData = []
+        text = table_ETH.loc[1, 2].replace("\n", " ")
+        addressData.append(text.split()[0])
+        addressData.append(text.split()[1])
+        arrayMessageAddress_ETH.append(addressData)
+
+# Функция создания массива с полезными данными ETH сообщений
+def GetArrayMessageData_MKIO(df_MKIO):
+    for i, table_MKIO in enumerate(df_MKIO):
+        data_rows = []
+        for row in range(4, len(table_MKIO)):
+            data_cells = []
+            for cell in range(1, 3):
+                data_cells.append(table_MKIO.loc[row, cell])
+            data_rows.append(data_cells)
+        arrayMessageData_MKIO.append(data_rows)
+
+# Функция создания массива с полезными данными ETH сообщений
+def GetArrayMessageData_ETH(df_ETH):
+    for i, table_ETH in enumerate(df_ETH):
+        data_rows = []
+        for row in range(4, len(table_ETH)):
+            data_cells = []
+            for cell in range(1, 3):
+                data_cells.append(table_ETH.loc[row, cell])
+            data_rows.append(data_cells)
+        arrayMessageData_ETH.append(data_rows)
+
+
 
 
 
@@ -102,7 +151,7 @@ for i, table in enumerate(doc.tables):
 # Флаги для МКИО и Eth
 flag_MKIO = False
 flag_ETH = False
-# Цикл по всем DataFrame pandas
+# Цикл по всем DataFrame pandas для создания массивов таблиц МКИО и ETH
 for i, table in enumerate(df):
     # Цикл для строк
     for index, row in table.iterrows():
@@ -126,22 +175,89 @@ for i, table in enumerate(df):
             break
         if flag_ETH == True:
             df_ETH.append(table)
-            flag_MKIO = False
+            flag_ETH = False
             break
 
 
-
-print("---------------------------------------------------------")
 
 GetArrayMessageNames_MKIO(df_MKIO)
 GetArrayMessageNames_ETH(df_ETH)
 
 
+GetArrayMessageAddress_MKIO(df_MKIO)
+GetArrayMessageAddress_ETH(df_ETH)
 
 
-print(arrayMessageNames_MKIO)
+GetArrayMessageData_MKIO(df_MKIO)
+GetArrayMessageData_ETH(df_ETH)
+
+
+print("---------------------------------------------------------")
+
+
+#print(arrayMessageAddress_MKIO)
+#print(arrayMessageAddress_ETH)
+
+
+print("---------------------------------------------------------")
+
+
+#print(arrayMessageNames_MKIO)
+#print(arrayMessageNames_ETH)
+
+
+print("---------------------------------------------------------")
+
+
+
+
+
+'''''
+for i in range(len(df_MKIO)):
+    data = []
+    data.append(arrayMessageNames_MKIO[i])
+    data.append(arrayMessageAddress_MKIO[i])
+    data.append(arrayMessageData_MKIO[i])
+    df_Total_MKIO.append(data)
+
+
+for i in range(len(df_ETH)):
+    data = []
+    data.append(arrayMessageNames_ETH[i])
+    data.append(arrayMessageAddress_ETH[i])
+    data.append(arrayMessageData_ETH[i])
+    df_Total_ETH.append(data)
+'''''
+
+
+
+
+
+#print(arrayMessageData_MKIO)
+
+
+print("---------------------------------------------------------")
+
+
+print(df_Total_MKIO)
+print(df_Total_ETH)
+
+
+
 
 
 # Сохраняем DataFrame в CSV-файл
 for i, table in enumerate(df_MKIO):
-    table.to_csv(f'new_table_{i+1}.csv', encoding='windows-1251', index=False, sep=';')
+    table.to_csv(f'MKIO_table_{i+1}.csv', encoding='windows-1251', index=False, sep=';')
+
+for i, table in enumerate(df_ETH):
+    table.to_csv(f'ETH_table_{i+1}.csv', encoding='windows-1251', index=False, sep=';')
+
+
+'''''
+for i, table in enumerate(df_Total_MKIO):
+    table.to_csv(f'Total_MKIO_table_{i+1}.csv', encoding='windows-1251', index=False, sep=';')
+
+for i, table in enumerate(df_Total_ETH):
+    table.to_csv(f'Total_ETH_table_{i+1}.csv', encoding='windows-1251', index=False, sep=';')
+'''''
